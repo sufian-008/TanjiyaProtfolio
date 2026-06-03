278614f71946bcaead9f765d745fb7778af18f68
sufian-008 <m.a.sufian008@gmail.com>
2026-06-03 17:02:02 +0600

    Enable static export for nextjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['localhost', 'res.cloudinary.com', 'images.unsplash.com'],
   },
 }
 
 module.exports = nextConfig
+
